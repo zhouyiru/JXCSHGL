a4544041d13845e54b17ddbcff0bf8efe132a9d6
CoderZhou <zyr40318076@163.com>
2019-11-19 00:03:17 +0800

    测试贾总问题

--- src/com/fh/dao/DaoSupport.java
+++ src/com/fh/dao/DaoSupport.java
@@ -121,7 +121,11 @@ public class DaoSupport implements DAO {
 	public Object findForMap(String str, Object obj, String key, String value) throws Exception {
 		return sqlSessionTemplate.selectMap(str, obj, key);
 	}
-	
+
+	private void testZyr(){
+		int a = 0;
+		a++;
+	}
 }
 
 
